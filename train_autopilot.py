918eb3312c761f3a8fd7181decbac9a206ab7219
catalinhrg <cherghelegiu11@gmail.com>
2017-12-02 00:38:41 +0200

    re wrote the traiig procedure and added accuracy evaluation

--- train_autopilot.py
+++ train_autopilot.py
@@ -13,6 +13,9 @@ label_size = dsm.HDF5_DataSet.get_label_size()
 epochs = 50
 mini_batch_size = 100
 
+tr_batches = tr_size / 100
+te_batches = te_size / 100
+
 # MLP with one hidden layer
 input_size = sample_size
 hidden_units = 100
@@ -33,16 +36,19 @@ def feedforward(x) :
 	l1 = tf.nn.relu(l1)
 
 	output = tf.add(tf.matmul(l1, w_o), b_o)
-	output = tf.nn.sigmoid(output)
 
 	return output
 
 prediction = feedforward(x)
-#cost = tf.reduce_mean(tf.pow(tf.subtract(prediction, y), 2))
-cost = tf.losses.absolute_difference(y, prediction) 
+
+cost = tf.nn.softmax_cross_entropy_with_logits(lables = y, logits = prediction, name = 'cost')
 optimizer = tf.train.AdamOptimizer().minimize(cost)
 
+accuracy = tf.reduce_mean( tf.cast( tf.equal( tf.argmax(prediction), tf.argmax(y) ), 'int'), name = 'accuracy' )
+
 file_reader = dsm.HDF5_Reader('DataSet.h5')
+
+tr_accuracy = te_accuracy = 0
 
 with tf.Session() as sess :
 
@@ -54,7 +60,7 @@ with tf.Session() as sess :
 		epoch_cost = 0
 		starting_point = 0
 
-		for batch in range(tr_size / mini_batch_size) :
+		for batch in range(tr_batches) :
 
 			sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'train')
 			_, c = sess.run( [optimizer, cost], {x: sample_batch, y: label_batch} )
@@ -63,10 +69,24 @@ with tf.Session() as sess :
 			epoch_cost += c
 
 		print 'Epoch ', epoch + 1, ' cost ',  epoch_cost
+		print 'Finished training in ', time() - start
 
-	print 'finished training in ', time() - start
+		starting_point = 0
 
-	# TODO evaluate accuracy
+		for batch in range( tr_batches ) :
+
+			sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'train')
+			tr_accuracy = tr_accuracy + accuracy.eval( { x: sample_batch, y: label_batch } )
+
+			if( batch < te_batches ) :
+
+				sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'test')
+				te_accuracy = te_accuracy + accuracy.eval( { x: sample_batch, y: label_bitch } )
+
+		tr_accuracy = tr_accuracy / tr_batches
+		te_accuracy = te_accuracy / te_batches
+
+		print 'Training accuracy : ', tr_accuracy, ' Test accuracy :  ', te_accuracy
 
 	saver = tf.train.Saver()
 	saver.save(sess, 'models/model.ckpt')
