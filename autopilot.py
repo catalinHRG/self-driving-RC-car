72317a70417602d12d36d3fccdb657af869188e2
catalinhrg <cherghelegiu11@gmail.com>
2017-12-04 01:35:35 +0200

    generalized tf implementation and added tensorboard functionality

--- autopilot.py
+++ autopilot.py
@@ -5,9 +5,8 @@ import sys
 
 from utils import connectivity_manager as cm
 from utils import data_set_manager as dsm
+from utils import model as md
 from time import time, sleep
-
-# laying out TF model
 
 client_ip_address = sys.argv[1]
 port = int( sys.argv[2] )
@@ -16,61 +15,18 @@ input_layer_size = dsm.HDF5_DataSet.get_sample_size()
 hidden_layer_size = 150
 output_layer_size = dsm.HDF5_DataSet.get_label_size()
 
+# laying out TF model
+
 x = tf.placeholder('float', [None, input_layer_size])
 
-w1 = tf.Variable(
-
-	tf.zeros( [input_layer_size, hidden_layer_size] ),
-	dtype = tf.float32,
-	name = 'w1'
-
-	)
-
-b1 = tf.Variable(
-
-	tf.zeros( [hidden_layer_size] ),
-	dtype = tf.float32,
-	name = 'b1'
-
-	)
-
-w_o = tf.Variable(
-
-	tf.zeros( [hidden_layer_size, output_layer_size] ),
-	dtype = tf.float32,
-	name = 'w_o'
-
-	)
-
-b_o = tf.Variable(
-
-	tf.zeros( [output_layer_size] ),
-	dtype = tf.float32,
-	name = 'b_o'
-
-	)
-
-def autopilot(x):
-
-	first_layer = tf.add(tf.matmul(x, w1), b1)
-	first_layer = tf.nn.relu(first_layer)
-
-	output_layer = tf.add(tf.matmul(first_layer, w_o), w_o)
-	output_layer = tf.nn.softmax( output_layer )
-
-	max_index = np.argmax( output_layer )
-	prediction = np.zeros( output_layer_size, dtype = np.uint8 )
-	np.put( prediction, max_index, 1 )
-
-	return prediction # one-hot encoded vector
-
-compute_prediction = autopilot(x)
+l1 = md.fully_connected_layer(x, input_layer_size, hidden_layer_size, 'l1')
+prediction = md.fully_connected_layer(l1, hidden_layer_size, output_layer_size, 'logits', act = tf.identity)
 
 # loading TF model from disk
 
 with tf.Session() as sess :
 
-	sess.run(tf.global_variables_initializer())
+	sess.run( tf.global_variables_initializer() )
 	saver = tf.train.Saver()
 	saver.restore(sess, "models/model.ckpt")
 
--- train_autopilot.py
+++ train_autopilot.py
@@ -3,9 +3,10 @@ import numpy as np
 import sys
 
 from utils import data_set_manager as dsm
+from utils import model as md
+
 from time import time
 
-# Global variables
 tr_size = dsm.HDF5_DataSet.get_tr_set_size()
 te_size = dsm.HDF5_DataSet.get_te_set_size()
 sample_size = dsm.HDF5_DataSet.get_sample_size()
@@ -14,82 +15,66 @@ label_size = dsm.HDF5_DataSet.get_label_size()
 epochs = int( sys.argv[1] )
 mini_batch_size = int( sys.argv[2] )
 
-tr_accuracy = te_accuracy = 0
-
 file_reader = dsm.HDF5_Reader('utils/DataSet.h5')
 
-tr_batches = tr_size / 100
-te_batches = te_size / 100
+tr_batches = tr_size / mini_batch_size
+te_batches = te_size / mini_batch_size
 
-# MLP with one hidden layer
 input_size = sample_size
-hidden_units = 150
+h1_size = 150
 output_size = label_size
 
-x = tf.placeholder('float', shape = [None, input_size], name = 'input')
-y = tf.placeholder('float', shape = [None, output_size], name = 'output')
+x = tf.placeholder( dtype = 'float' , shape = [None, input_size] , name = 'input')
+y = tf.placeholder( dtype = 'float' , shape = [None, output_size] , name = 'output')
 
-w1 = tf.Variable(tf.random_normal([input_size, hidden_units]), name = 'w1')
-b1 = tf.Variable(tf.random_normal([hidden_units]), name = 'b1')
+l1 = md.fully_connected_layer(x, input_size, h1_size, 'l1')
+logits = md.fully_connected_layer(l1, h1_size, output_size, 'logits', tf.identity)
 
-w_o = tf.Variable(tf.random_normal([hidden_units, output_size]), name = 'w_o')
-b_o = tf.Variable(tf.random_normal([output_size]), name = 'b_o')
+cost = md.build_cost_node(logits, y, 'cost')
 
-def feedforward(x) :
-
-	l1 = tf.add(tf.matmul(x, w1), b1)
-	l1 = tf.nn.relu(l1)
-
-	output = tf.add(tf.matmul(l1, w_o), b_o)
-
-	return output
-
-prediction = feedforward(x)
-
-cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = prediction, name = 'cost') )
 optimizer = tf.train.AdamOptimizer( name = 'optimizer' ).minimize(cost)
-accuracy = tf.reduce_mean( tf.cast( tf.equal( tf.argmax(prediction), tf.argmax(y) ), dtype = tf.int32), name = 'accuracy' )
+
+accuracy = md.build_accuracy_node(logits, y, 'accuracy')
+
+merged_summary_ops = tf.summary.merge_all()
+summary_writer = tf.summary.FileWriter('tensorboard_data/')
 
 with tf.Session() as sess :
 
-	sess.run(tf.global_variables_initializer())
+	sess.run( tf.global_variables_initializer() )
 
 	for epoch in range(epochs) :
 
 		start = time()
 
-		epoch_cost = 0
 		starting_point = 0
 
 		for batch in range( tr_batches ) :
 
 			sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'train')
-			_, c = sess.run( [optimizer, cost], {x: sample_batch, y: label_batch} )
+			summary, _, _ = sess.run( [merged_summary_ops, optimizer, cost], {x: sample_batch, y: label_batch} )
+
+			summary_writer.add_summary(summary, batch)
 
 			starting_point += mini_batch_size
-			epoch_cost += c
 
-		print 'Epoch ', epoch + 1, ' cost ',  epoch_cost
-		print 'Finished training in ', time() - start
+		print 'Epoch ', epoch + 1, ' finished in ', time() - start
 
 		starting_point = 0
 
 		for batch in range( tr_batches ) :
 
 			sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'train')
-			tr_accuracy = tr_accuracy + accuracy.eval( { x: sample_batch, y: label_batch } )
+			summary, acc = sess.run( [merged_summary_ops, accuracy], feed_dict = { x: sample_batch, y: label_batch } )
 
 			if( batch < te_batches ) :
 
 				sample_batch, label_batch = file_reader.next_batch(starting_point, mini_batch_size, 'test')
-				te_accuracy = te_accuracy + accuracy.eval( { x: sample_batch, y: label_batch } )
+				summary, acc = sess.run( [merged_summary_ops, accuracy], feed_dict = { x: sample_batch, y: label_batch } )
 
 			starting_point += mini_batch_size
 
-		tr_accuracy = tr_accuracy / tr_batches
-		te_accuracy = te_accuracy / te_batches
-
-		print 'Training set accuracy : ', tr_accuracy, ' Test set accuracy :  ', te_accuracy
+	summary_writer.close()
 
 	saver = tf.train.Saver()
 	saver.save(sess, 'models/model.ckpt')
